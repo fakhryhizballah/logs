ae801ee2aadc5dc1f8cbd98e6ae35f42705673b1
fakhryhizballah <falehry88@gmail.com>
2025-01-11 11:28:22 +0700

    cros

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
 const routes = require('./routes');
-
+const cors = require('cors')
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const uri = process.env.MONGO_URI;
 
