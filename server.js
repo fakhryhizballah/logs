e167739506f3a2e5ac4f4ee66b3a074a1ba4f392
fakhryhizballah <falehry88@gmail.com>
2025-01-11 11:39:39 +0700

    add morgan

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const routes = require('./routes');
+const morgan = require('morgan');
 const cors = require('cors')
 const app = express();
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(cors());
 
 const uri = process.env.MONGO_URI;
